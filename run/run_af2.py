55d17249d699714561919ea634ad22a2e9a1f628
Nick Randolph (Laptop) <nzrandol@unc.edu>
2021-11-07 21:10:39 -0500

    Added arguments for random seeds and number of ensembles.

--- run/run_af2.py
+++ run/run_af2.py
@@ -1,3 +1,5 @@
+""" Full AlphaFold protein structure prediction script."""
+
 import jax
 print(f'Running with {jax.local_devices()[0].device_kind} '
       '{jax.local_devices()[0].platform.upper()}')
@@ -51,6 +53,11 @@ raw_inputs_from_sequence = getMultimerRawInputs(
     output_dir=args.output_dir,
     raw_inputs=raw_inputs_from_sequence)
 
+# Get random seeds.
+seeds = getRandomSeeds(
+    random_seed=args.random_seed,
+    num_seeds=args.num_seeds)
+
 # Get model names.
 monomer_model_names = ()
 if len(monomer_queries) > 0:
@@ -62,10 +69,16 @@ if len(multimer_queries) > 0:
     multimer_model_names = getModelNames(
         mode='multimer', num_models=args.num_models)
 
+if args.use_amber:
+    amber_relaxer = relax.AmberRelaxation(
+        max_iterations=0, tolerance=2.39, stiffness=10.0,
+        exclude_residues=[], max_outer_iterations=3)
+    
 # Predict structures.
 for model_name in monomer_model_names + multimer_model_names:
         model_runner = getModelRunner(
-            model_name=model_name)
+            model_name=model_name,
+            num_ensemble=args.num_ensemble)
 
         for query in monomer_queries + multimer_queries:
             filename = query[0]
@@ -89,48 +102,50 @@ for model_name in monomer_model_names + multimer_model_names:
                 sequences=sequences,
                 chain_features=features_for_chain,
                 is_prokaryote=args.is_prokaryote)
-            
-            result = predictStructure(
-                model_runner=model_runner,
-                feature_dict=input_features,
-                model_type='monomer' if 'monomer' in model_name else 'multimer')
-
-            if args.dont_write_pdbs:
-                unrelaxed_pdb = protein.to_pdb(result('unrelaxed_protein'])
-
-                unrelaxed_pred_path = os.path.join(
-                    args.output_dir, 'unrelaxed_prediction.pdb')
-                with open(unrelaxed_pred_path, 'w') as f:
-                    f.write(unrelaxed_pdb)
-
-                del unrelaxed_pdb, unrelaxed_pred_path
-            
-            if args.use_amber:
-                amber_relaxer = relax.AmberRelaxation(
-                    max_iterations=0,
-                    tolerance=2.39,
-                    stiffness=10.0,
-                    exclude_residues=[],
-                    max_outer_iterations=3)
-                relaxed_pdb, _, _ = amber_relaxer.process(
-                    prot=result['unrelaxed_protein'])
-
-                if not args.dont_write_pdbs:
-                    relaxed_pred_path = os.path.join(
-                        args.output_dir, 'relaxed_prediction.pdb')
-                    with open(relaxed_pred_path, 'w') as f:
-                        f.write(relaxed_pdb)
-
-                    del relaxed_pred_path
-
-                del amber_relaxer, relaxed_pdb
-
-            if args.compress_output:
-                results_path = os.path.join(
-                    args.output_dir, 'results')
-                utils.compressed_pickle(results_path, results)
-
-                del results_path
 
             del filename, custom_a3m, sequences, features_for_chain
-            del input_features, result
+
+            for seed in seeds:
+                if 'monomer' in model_name:
+                    model_type = 'monomer'
+                else:
+                    model_type = 'multimer'
+
+                result = predictStructure(
+                    model_runner=model_runner,
+                    feature_dict=input_features,
+                    model_type=model_type,
+                    random_seed=seed)
+
+                if args.dont_write_pdbs:
+                    unrelaxed_pdb = protein.to_pdb(result('unrelaxed_protein'])
+
+                    unrelaxed_pred_path = os.path.join(
+                        args.output_dir, 'unrelaxed_prediction.pdb')
+                    with open(unrelaxed_pred_path, 'w') as f:
+                        f.write(unrelaxed_pdb)
+
+                        del unrelaxed_pdb, unrelaxed_pred_path
+            
+                if args.use_amber:
+                    relaxed_pdb, _, _ = amber_relaxer.process(
+                        prot=result['unrelaxed_protein'])
+
+                    if not args.dont_write_pdbs:
+                        relaxed_pred_path = os.path.join(
+                            args.output_dir, 'relaxed_prediction.pdb')
+                        with open(relaxed_pred_path, 'w') as f:
+                            f.write(relaxed_pdb)
+
+                        del relaxed_pred_path
+
+                    del relaxed_pdb
+
+                if args.compress_output:
+                    results_path = os.path.join(
+                        args.output_dir, 'results')
+                    utils.compressed_pickle(results_path, results)
+
+                    del results_path
+
+                del result
