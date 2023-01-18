42bd9b4b69dc6eba569181e03c3ee7b586d1c948
Nick Randolph (longleaf) <nzrandol@unc.edu>
2023-01-18 16:00:51 -0500

    Updated flags for multimer v2 and added new example for v3

---
+++ examples/homodimer/v2/run_af2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH -J af2_homodimer
+#SBATCH -p volta-gpu
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --mem=16g
+#SBATCH -t 03-00:00:00
+#SBATCH --qos gpu_access
+#SBATCH --gres=gpu:1
+#SBATCH --output=slurm-%j-%x.out
+#SBATCH --error=slurm-%j-%x.out
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=nzrandol@unc.edu
+
+source ~/.bashrc
+conda activate af2
+python /proj/kuhl_lab/alphafold/run/run_af2.py @flags/flags_longleaf.txt
